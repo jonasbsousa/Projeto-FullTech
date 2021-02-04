331bc660f7d7164e8b7d23d1cb4add60e0e8713b
JONAS BIZERRA DE SOUSA <jonas.bsousa@senacsp.edu.br>
2021-02-04 19:22:23 -0300

    Versão1.0

---
+++ Models/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Senac.Models
+{
+    public class ApplicationSettings
+    {
+        public bool AutoConfirmEmail { get; set; }
+    }
+}
